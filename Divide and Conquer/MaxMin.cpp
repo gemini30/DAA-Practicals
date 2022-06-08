95a543816cb856780afd03ee552f822fad97e341
Jash <jashshah3005@gmail.com>
2022-06-08 09:54:21 +0530

    Added karatsuba, strassen, LCS and mcm

--- Divide and Conquer/MaxMin.cpp
+++ Divide and Conquer/MaxMin.cpp
@@ -1,69 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void findMinMax(vector<int> &nums, int low, int high, int &max, int &min)
+void minmax(vector<int> &arr, int low, int high, int &mini, int &maxi)
 {
-
     if (low == high)
-    {
-        if (nums[low] < min)
-        {
-            min = nums[low];
-        }
-        if (nums[low] > max)
-        {
-            max = nums[low];
-        }
         return;
-    }
-
-    if (low == high - 1)
+    else if (high - low == 1)
     {
-        if (nums[low] < nums[high])
+        if (arr[low] > arr[high])
         {
-            if (nums[low] < min)
-            {
-                min = nums[low];
-            }
-            if (nums[low] > max)
-            {
-                max = nums[low];
-            }
+            mini = min(mini, arr[high]);
+            maxi = max(maxi, arr[low]);
         }
         else
         {
-            if (nums[low] < min)
-            {
-                min = nums[low];
-            }
-            if (nums[low] > max)
-            {
-                max = nums[low];
-            }
+            mini = min(mini, arr[low]);
+            maxi = max(maxi, arr[high]);
         }
-
-      return;  
     }
-
-    int mid = low+high/2;
-    findMinMax(nums,low,mid,max,min);
-    findMinMax(nums,mid+1,high,max,min);
+    else
+    {
+        int mid = (low + high) / 2;
+        minmax(arr, low, mid, mini, maxi);
+        minmax(arr, mid + 1, high, mini, maxi);
+    }
+    return;
 }
 
-int main(){
+int main()
+{
+
     int n;
-    cout<<"Enter size of array: "<<endl;
-    cin>>n;
-    cout<<"Enter array elements: "<<endl;
-    vector<int> nums(n);
-     for (auto &it : nums)
+    cin >> n;
+    vector<int> arr(n);
+
+    for (auto &it : arr)
     {
         cin >> it;
     }
-    int min = INT_MAX, max = INT_MIN;
-    findMinMax(nums,0,n-1,max,min);
-    cout<<"Max element is: "<<max<<endl;
-    cout<<"Min element is: "<<min;
-    
+
+    int mini = 1e9;
+    int maxi = -1e9;
+
+    int low = 0;
+    int high = n - 1;
+
+    minmax(arr, low, high, mini, maxi);
+
+    cout << mini << " " << maxi << endl;
+
     return 0;
 }
